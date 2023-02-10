eae7c0ee1051f57f1851a9eeb0fb0e1fd96b669b
g0dgar1cc <ting@s.sfusd.edu>
2023-02-10 15:39:13 -0800

    added code to stop things from moving on game over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerControls playerControlsScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControlsScript = GameObject.Find("Player").GetComponent<PlayerControls>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControlsScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    
     }
 }
